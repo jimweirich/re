0b40ba81ae15b5ba6295a5642c470c588bbb4611
Jim Weirich <jim.weirich@gmail.com>
2009-12-30 17:57:36 -0500

    Updated comments.

--- lib/re.rb
+++ lib/re.rb
@@ -9,7 +9,7 @@
 #
 #    /\A((?:19|20)[0-9]{2})[\- \/.](0[1-9]|1[012])[\- \/.](0[1-9]|[12][0-9]|3[01])\z/
 #
-# Using the Re library, That regular expression can be built
+# Using the Re library, that regular expression can be built
 # incrementaly from smaller, easier to understand expressions.
 # Perhaps something like this:
 #
